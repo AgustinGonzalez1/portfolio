cd48fc67f17c8cc17b58d20c1849da8bdae15173
Agustin Gonzalez <agustin_.gonzalez_@hotmail.com>
2024-02-04 20:16:59 -0300

    remove .

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -7,42 +7,42 @@ export interface Project {
 const projects: Project[] = [
 	{
 		title: "Proyecto Final de Desarrollo Web",
-		img: "./public/img_projects/guaridaGamer.jpg",
+		img: "/public/img_projects/guaridaGamer.jpg",
 		link: "https://guarida-gamer.netlify.app/",
 	},
 	{
 		title: "Proyecto Final de JavaScript 1",
-		img: "./public/img_projects/javaScriptn1.jpg",
+		img: "/public/img_projects/javaScriptn1.jpg",
 		link: "https://tienda-el-clima.netlify.app/",
 	},
 	{
 		title: "Proyecto Final de JavaScript 2",
-		img: "./public/img_projects/javaScriptn2.jpg",
+		img: "/public/img_projects/javaScriptn2.jpg",
 		link: "https://el-clima-app.netlify.app/",
 	},
 	{
 		title: "Proyecto Final de React",
-		img: "./public/img_projects/react.jpg",
+		img: "/public/img_projects/react.jpg",
 		link: "https://sportify-tienda.netlify.app/",
 	},
 	{
 		title: "Landing Page: Muebles a medida",
-		img: "./public/img_projects/muebles.jpg",
+		img: "/public/img_projects/muebles.jpg",
 		link: "https://muebles-a-medida.netlify.app/",
 	},
 	{
 		title: "TaskMaster",
-		img: "./public/img_projects/taskMaster.jpg",
+		img: "/public/img_projects/taskMaster.jpg",
 		link: "https://taskmasterhub.vercel.app/",
 	},
 	{
 		title: "Corona",
-		img: "./public/img_projects/corona.jpg",
+		img: "/public/img_projects/corona.jpg",
 		link: "https://corona-ecommerce.vercel.app/",
 	},
 	{
 		title: "Rastrea Patitas",
-		img: "./public/img_projects/rastreaPatitas.jpg",
+		img: "/public/img_projects/rastreaPatitas.jpg",
 		link: "https://rastreapatitas.vercel.app/",
 	},
 ];
